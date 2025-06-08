2dbc891b76431ae76c3a8ab5706ccaa369817b4b
Benson Liu <bensonhliu@gmail.com>
2025-06-07 18:53:52 -0700

    commented out

--- api/views/compile.py
+++ api/views/compile.py
@@ -1,26 +1,26 @@
-from flask import Blueprint
+# from flask import Blueprint
 
-from flask_login import login_required, current_user
+# from flask_login import login_required, current_user
 
-from sqlalchemy import select, and_, func, delete
+# from sqlalchemy import select, and_, func, delete
 
-from models.resume import Resume, ResumeSection, ResumeItem, ResumeAssociation
+# from models.resume import Resume, ResumeSection, ResumeItem, ResumeAssociation
 
-from db import db
+# from db import db
 
-import requests
+# import requests
 
-compile_view = Blueprint("compile_view", __name__, url_prefix="/compile")
+# compile_view = Blueprint("compile_view", __name__, url_prefix="/compile")
 
-TEXIFY_URL = "http://localhost:9090"
+# TEXIFY_URL = "http://localhost:9090"
 
-@compile_view.post('/<int:resume_id>')
-@login_required
-def compile_resume(resume_id: int):
-    stmt = select(Resume).where(and_(Resume.user_id == current_user.id, Resume.id == id))
-    result = db.session.execute(stmt)
-    result = result.scalar()
-    if not result:
-        return {"error": "Resume not found"}, 404
+# @compile_view.post('/<int:resume_id>')
+# @login_required
+# def compile_resume(resume_id: int):
+#     stmt = select(Resume).where(and_(Resume.user_id == current_user.id, Resume.id == id))
+#     result = db.session.execute(stmt)
+#     result = result.scalar()
+#     if not result:
+#         return {"error": "Resume not found"}, 404
 
-    r = requests.post(TEXIFY_URL + "/")
+#     r = requests.post(TEXIFY_URL + "/")
