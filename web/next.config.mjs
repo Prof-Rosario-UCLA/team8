81d206fc1d3b3ddafa78710595cbfc2e47b271ab
Benson Liu <bensonhliu@gmail.com>
2025-06-08 16:51:57 -0700

    added client web to docker-compose

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,11 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   // Add or modify the rewrites configuration
   async rewrites() {
+    const baseUrl = process.env.SERVER_URL || 'http://127.0.0.1:5001';
+  
     return [
       {
         source: '/api/:path*',
         // Proxy to the Flask app (5001 in our scripts)
-        destination: 'http://127.0.0.1:5001/api/:path*',
+        destination: baseUrl + '/api/:path*',
       },
     ];
   },
