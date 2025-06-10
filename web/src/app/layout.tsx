2422ac69ef41e075dd221b43b5899158dc752cc1
Leon Liu <leondev360@gmail.com>
2025-06-10 16:23:40 -0700

    feat: remove auth from home

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { AuthProvider } from '@/auth/AuthProvider';
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -34,9 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
-        <AuthProvider>
           {children}
-        </AuthProvider>
       </body>
     </html>
   );
